e47fd07940b6982007204b9bb16b846af35b71c0
Dániel Zolnai <zoiddani@gmail.com>
2016-01-08 19:34:37 +0100

    Windows Forms working

--- csharp/library/SurfnetSSO/SurfnetSSO.Platform.Net45/WinFormsSSOManager.cs
+++ csharp/library/SurfnetSSO/SurfnetSSO.Platform.Net45/WinFormsSSOManager.cs
@@ -2,17 +2,34 @@
 using System.Windows.Forms;
 
 namespace SurfnetSSO {
+    /// <summary>
+    /// !!! Important !!!
+    /// Do NOT use callback URLs for pages which do not exist. For optimal use, redirect to an URL that you own.
+    /// For more info, see: http://davidquail.com/2010/01/10/c-webbrowser-swallows-redirect-uri/
+    /// </summary>
     public class WinFormsSSOManager : SSOManager {
 
-        public static void authorize(WebBrowser webForm, String consumerId, String endpoint, String scheme) {
-            Uri uri = authorize(consumerId, endpoint, scheme);
-            webForm.Navigate(uri.ToString());
-            webForm.DocumentCompleted += (sender, e) => {
-                Console.WriteLine(e.Url.ToString());
-                if (isCallbackUrl(e.Url, scheme)) {
-                    OnAuthorizationFinished(AuthorizationEventArgs.Success("lalala"));
-                }
+        public static void authorize(WebBrowser webBrowser, String consumerId, String endpoint, String callbackUrl) {
+            Uri uri = authorize(consumerId, endpoint, callbackUrl);
+
+            webBrowser.LocationChanged += (sender, e) => {
+                _webLocationChanged(webBrowser.Url, callbackUrl);
             };
+            webBrowser.Navigating += (sender, e) => {
+                _webLocationChanged(e.Url, callbackUrl);
+            };
+            webBrowser.Navigated += (sender, e) => {
+                _webLocationChanged(e.Url, callbackUrl);
+            };
+
+            webBrowser.Navigate(uri.AbsoluteUri);
+        }
+
+        private static void _webLocationChanged(Uri uri, String callbackUrl) {
+            var args = extractArgsFromUrlWhenReady(uri.AbsoluteUri, callbackUrl);
+            if (args != null) {
+                OnAuthorizationFinished(args);
+            }
         }
     }
 }
--- csharp/library/SurfnetSSO/SurfnetSSO/SSOManager.cs
+++ csharp/library/SurfnetSSO/SurfnetSSO/SSOManager.cs
@@ -13,30 +13,53 @@ namespace SurfnetSSO {
         /// <param name="consumerId">The consumer ID of this application</param>
         /// <param name="endpoint">The URL of the server</param>
         /// <param name="endpoint">The path relative to the URL which points to the authentication page</param>
-        /// <param name="scheme">The callback scheme which will be invoked by the server at the end of the flow</param>
-        public static Uri authorize(String consumerId, String endpoint, String scheme) {
-            if (String.IsNullOrEmpty(consumerId)) {
+        /// <param name="callbackUrl">The callback URL which will be invoked by the server at the end of the flow</param>
+        public static Uri authorize(string consumerId, string endpoint, string callbackUrl) {
+            if (string.IsNullOrEmpty(consumerId)) {
                 throw new ArgumentNullException("Consumer ID can not be empty!");
             }
-            if (String.IsNullOrEmpty(endpoint)) {
+            if (string.IsNullOrEmpty(endpoint)) {
                 throw new ArgumentNullException("Endpoint can not be empty!");
             }
-            if (String.IsNullOrEmpty(scheme)) {
+            if (string.IsNullOrEmpty(callbackUrl)) {
                 throw new ArgumentNullException("Scheme can not be empty!");
             }
-            String url = _addQueryParameter(endpoint, "client_id", consumerId);
+            string url = _addQueryParameter(endpoint, "client_id", consumerId);
             url = _addQueryParameter(url, "response_type", "token");
             url = _addQueryParameter(url, "state", "surfnet");
             url = _addQueryParameter(url, "scope", "authorize");
             return new Uri(url, UriKind.Absolute);
         }
 
-        public static bool isCallbackUrl(Uri uri, String scheme) {
-            return uri.Scheme.Equals(scheme);
+        public static bool isCallbackUrl(string url, string callbackUrl) {
+            return url.StartsWith(callbackUrl);
         }
 
-        private static String _addQueryParameter(String url, String key, String value) {
-            String result = url;
+        protected static AuthorizationEventArgs extractArgsFromUrlWhenReady(string url, string callbackUrl) {
+            if (isCallbackUrl(url, callbackUrl)) {
+                url = url.Replace(callbackUrl, "");
+                url = url.Substring(1); // Remove leading # or ?
+                var queryParams = url.Split('&');
+                string token = null, errorType = null, errorMessage = null;
+                foreach (var query in queryParams) {
+                    if (query.StartsWith("error=")) {
+                        errorType = query.Replace("error=", "");
+                    } else if (query.StartsWith("error_description")) {
+                        errorMessage = query.Replace("error_description=", "");
+                    } else if (query.StartsWith("access_token=")) {
+                        token = query.Replace("access_token=", "");
+                    }
+                }
+                if (token != null) {
+                    return AuthorizationEventArgs.Success(token);
+                } else {
+                    return AuthorizationEventArgs.Error(errorType, errorMessage);
+                }
+            }
+            return null;
+        }
+        private static string _addQueryParameter(string url, string key, string value) {
+            string result = url;
             if (url.Contains("?")) {
                 result += "&"; 
             } else {
